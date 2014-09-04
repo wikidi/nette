8923e991bb779afcb718298a06c470b83f57e7d0
Komanc <jirka@czechgeeks.cz>
2014-09-04 12:27:05 +0200

    netteforms.js: Change order of showing error message and focusing element. In Firefox element lost focus when alert is invoked.

--- client-side/netteForms.js
+++ client-side/netteForms.js
@@ -167,11 +167,11 @@ Nette.isDisabled = function(elem) {
  * Display error message.
  */
 Nette.addError = function(elem, message) {
-	if (elem.focus) {
-		elem.focus();
-	}
 	if (message) {
 		alert(message);
+	}
+	if (elem.focus) {
+		elem.focus();
 	}
 };
 
